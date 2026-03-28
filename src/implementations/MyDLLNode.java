fd059e80595505583652d9a356f8f59dd174d12e
Joshua Couto <joshua.s.couto@gmail.com>
2026-03-28 15:22:51 -0600

    docs: updated file headers

--- src/implementations/MyDLLNode.java
+++ src/implementations/MyDLLNode.java
@@ -1,3 +1,16 @@
+/**
+ * MyDLLNode.java
+ * 
+ * @description A node class used internally by MyDLL. Each node 
+ * stores a single generic element along with references to both 
+ * the previous and next nodes in the doubly linked list.
+ * 
+ * @author Team Sidon - Aaron Reid, Cielo Pacot, Joshua Couto, Ryan Burns
+ * Course: CPRG-304 (Object-Oriented Programming 3)
+ * Institution: Southern Alberta Institute of Technology 
+ * Date: March 28th, 2026
+ */
+
 package implementations;
 
 public class MyDLLNode {
--- src/implementations/MyStack.java
+++ src/implementations/MyStack.java
@@ -1,3 +1,16 @@
+/**
+ * MyStack.java
+ * 
+ * @description A stack implementation of the StackADT interface, backed by a 
+ * MyArrayList. Follows Last-In-First-Out (LIFO) ordering, where elements are 
+ * pushed onto and popped from the top of the stack.
+ * 
+ * @author Team Sidon - Aaron Reid, Cielo Pacot, Joshua Couto, Ryan Burns
+ * Course: CPRG-304 (Object-Oriented Programming 3)
+ * Institution: Southern Alberta Institute of Technology 
+ * Date: March 28th, 2026
+ */
+
 package implementations;
 
 public class MyStack {
